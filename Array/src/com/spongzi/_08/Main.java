0ead85fe400448f3ea4b685a3bc75dadc81d81cb
Spongzi <lxlandsx@outlook.com>
2023-02-03 10:45:54 +0800

    剑指 Offer II 008. 和大于等于 target 的最短子数组

--- Array/src/com/spongzi/_08/Main.java
+++ Array/src/com/spongzi/_08/Main.java
@@ -14,14 +14,14 @@ public class Main {
     public int minSubArrayLen(int target, int[] nums) {
         int left = 0;
         int sum = 0;
-        int minLength = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
             while (left <= right && sum >= target) {
-                minLength = Math.min(minLength, right - left + 1);
+                ans = Math.min(ans, right - left + 1);
                 sum -= nums[left++];
             }
         }
-        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
